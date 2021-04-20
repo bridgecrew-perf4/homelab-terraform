c871391fee217d251408651058512edfdfd215bb
Anthony Rubiano <administrator@anthonyrubiano.com>
2021-04-20 15:57:13 -0400

    Update variables.tf

--- AWS/Terraform/variables.tf
+++ AWS/Terraform/variables.tf
@@ -1,22 +1,22 @@
 //Variable List
 variable "accessKey" {
-     default = "<ACCESS KEY>"
+  default = "<PUT IN YOUR AWS ACCESS KEY>"
 }
 variable "secretKey" {
-     default = "<SECRET KEY>"
+  default = "<PUT IN YOUR AWS SECRET KEY>"
 }
 variable "region" {
-     default = "us-east-1"
+  default = "us-east-1"
 }
 variable "availabilityZone" {
-     default = "us-east-1d"
+  default = "us-east-1d"
 }
 variable "vpcCIDRblock" {
-    default = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 variable "privateSubnetCIDRblock" {
-    default = "10.200.1.0/24"
+  default = "10.200.1.0/24"
 }
 variable "publicSubnetCIDRblock" {
-    default = "10.200.100.0/24"
+  default = "10.200.100.0/24"
 }
