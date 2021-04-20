60f13b04f197707ab718a22d74dd25f0a0525809
Anthony Rubiano <administrator@anthonyrubiano.com>
2021-04-20 15:57:36 -0400

    Update ec2.tf

--- AWS/Terraform/ec2.tf
+++ AWS/Terraform/ec2.tf
@@ -1,19 +1,21 @@
-//OpenVPN
+//OpenVPN EC2 Instance
 resource "aws_instance" "OpenVPN" {
-  ami = "ami-0742b4e673072066f"
+  ami           = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
+
   tags = {
-   Name = "OpenVPN"
-   Access = "Public"
- }
+    Name   = "OpenVPN"
+    Access = "Public"
+  }
 }
 
 //NAT Gateway
 resource "aws_instance" "NAT Gateway" {
-  ami = "ami-00a9d4a05375b2763"
+  ami           = "ami-00a9d4a05375b2763"
   instance_type = "t2.micro"
+
   tags = {
-   Name = "NAT Gateway"
-   Access = "Public"
- }
+    Name   = "NAT Gateway"
+    Access = "Public"
+  }
 }
