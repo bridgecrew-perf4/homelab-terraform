8314d8cd97df7bc0424b278d0f52f3fba0677b46
Anthony Rubiano <administrator@anthonyrubiano.com>
2021-04-20 12:12:02 -0400

    Update security_group.tf

--- AWS/Terraform/security_group.tf
+++ AWS/Terraform/security_group.tf
@@ -7,7 +7,7 @@ resource "aws_default_security_group" "default" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = ["aws_security_group.OpenVPN.id"]
+    security_groups = ["aws_security_group.openvpn.id"]
   }
 
   ingress {
@@ -15,7 +15,7 @@ resource "aws_default_security_group" "default" {
     from_port   = 7
     to_port     = 7
     protocol    = "tcp"
-    security_groups = ["aws_security_group.OpenVPN.id"]
+    security_groups = ["aws_security_group.openvpn.id"]
   }
 
   egress {
@@ -31,7 +31,7 @@ resource "aws_default_security_group" "default" {
 }
 
 //OpenVPN Security Group
-resource "aws_security_group" "OpenVPN" {
+resource "aws_security_group" "openvpn" {
   vpc_id      = aws_vpc.rubi-vpc.id
 
   ingress {
@@ -39,7 +39,46 @@ resource "aws_security_group" "OpenVPN" {
     from_port       = 1194
     to_port         = 1194
     protocol        = "udp"
-    }
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+//Nat Gateway Security Group
+resource "aws_security_group" "nat gateway" {
+  vpc_id      = aws_vpc.rubi-vpc.id
+
+  ingress {
+    description     = "HTTP"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+  }
+  
+  ingress {
+    description     = "HTTPS"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+  }
+  
+  ingress {
+    description     = "SMTPS"
+    from_port       = 465
+    to_port         = 465
+    protocol        = "tcp"
+  }
+  
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 }
