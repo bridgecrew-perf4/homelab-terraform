35ecd0c51ee169b8dfb592158b9da2a4cb681bee
Anthony Rubiano <administrator@anthonyrubiano.com>
2021-04-20 15:56:59 -0400

    Update subnets.tf

--- AWS/Terraform/subnets.tf
+++ AWS/Terraform/subnets.tf
@@ -1,19 +1,21 @@
 //Public Subnet
 resource "aws_subnet" "rubi-public-subnet" {
   vpc_id            = aws_vpc.rubi-vpc.id
-  cidr_block        = "var.publicSubnetCIDRblock"
-  availability_zone = "var.availabilityZone"
+  cidr_block        = var.publicSubnetCIDRblock
+  availability_zone = var.availabilityZone
+
   tags = {
-   Name = "rubi-public-subnet"
- }
+    Name = "rubi-public-subnet"
+  }
 }
 
 //Private Subnet
 resource "aws_subnet" "rubi-private-subnet" {
   vpc_id            = aws_vpc.rubi-vpc.id
-  cidr_block        = "var.privateSubnetCIDRblock"
-  availability_zone = "var.availabilityZone"
+  cidr_block        = var.privateSubnetCIDRblock
+  availability_zone = var.availabilityZone
+
   tags = {
-   Name = "rubi-private-subnet"
- }
+    Name = "rubi-private-subnet"
+  }
 }
