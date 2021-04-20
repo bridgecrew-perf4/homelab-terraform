ccd60b4aad9190bd4642580a9249bd21c252dd1d
Anthony Rubiano <administrator@anthonyrubiano.com>
2021-04-20 12:26:54 -0400

    Update security_group.tf

--- AWS/Terraform/security_group.tf
+++ AWS/Terraform/security_group.tf
@@ -58,6 +58,7 @@ resource "aws_security_group" "nat gateway" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
+    cidr_blocks     = ["var.privateSubnetCIDRblock"]
   }
   
   ingress {
@@ -65,6 +66,7 @@ resource "aws_security_group" "nat gateway" {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
+    cidr_blocks     = ["var.privateSubnetCIDRblock"]
   }
   
   ingress {
@@ -72,13 +74,31 @@ resource "aws_security_group" "nat gateway" {
     from_port       = 465
     to_port         = 465
     protocol        = "tcp"
+    cidr_blocks     = ["var.privateSubnetCIDRblock"]
   }
   
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "HTTP"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    description     = "HTTPS"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    description     = "SMTPS"
+    from_port       = 465
+    to_port         = 465
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
 }
