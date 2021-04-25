cdc0e0d0060d988795cd632e8ed2030ade1531e4
Anthony Rubiano <administrator@anthonyrubiano.com>
2021-04-25 14:17:43 -0400

    Added security group for bookstack

--- security_groups.tf
+++ security_groups.tf
@@ -113,4 +113,35 @@ resource "aws_security_group" "nat-gateway" {
   }
 }
 
-//TODO Add remaining SGs
+//BookStack Security Group
+resource "aws_security_group" "bookstack" {
+  description = "BookStack"
+  vpc_id      = aws_vpc.rubi-vpc.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.openvpn.id]
+  }
+
+  ingress {
+    description = "SMTPS"
+    from_port   = 465
+    to_port     = 465
+    protocol    = "tcp"
+    security_groups = [aws_security_group.nat-gateway.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "BookStack"
+  }
+}
