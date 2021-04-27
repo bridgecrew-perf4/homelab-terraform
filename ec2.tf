ee45de40fe6b34d23afd18598300afb2084016c1
Anthony Rubiano <administrator@anthonyrubiano.com>
2021-04-27 10:35:35 -0400

    Modified security group attachments

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "openvpn" {
   ami           = "ami-059f1cc52e6c85908"
   instance_type = "t2.micro"
+  security_groups = [aws_default_security_group.default.id, aws_security_group.openvpn.id]
   disable_api_termination = true
 
   tags = {
@@ -14,6 +15,7 @@ resource "aws_instance" "openvpn" {
 resource "aws_instance" "nat-gateway" {
   ami           = "ami-00a9d4a05375b2763"
   instance_type = "t2.micro"
+  security_groups = [aws_default_security_group.default.id, aws_security_group.nat-gateway.id]
   disable_api_termination = true
 
   tags = {
@@ -26,6 +28,7 @@ resource "aws_instance" "nat-gateway" {
 resource "aws_instance" "bookstack" {
   ami           = "ami-042e8287309f5df03"
   instance_type = "t2.micro"
+  security_groups = [aws_default_security_group.default.id, aws_security_group.bookstack.id]
   disable_api_termination = true
 
   tags = {
@@ -38,6 +41,7 @@ resource "aws_instance" "bookstack" {
 resource "aws_instance" "valheim" {
   ami           = "ami-059f1cc52e6c85908"
   instance_type = "t3.medium"
+  security_groups = [aws_default_security_group.default.id, aws_security_group.valheim.id]
   disable_api_termination = true
 
   tags = {
--- network_interfaces.tf
+++ network_interfaces.tf
@@ -8,12 +8,6 @@ resource "aws_network_interface" "openvpn" {
   }
 }
 
-//Attach OpenVPN Security Group
-resource "aws_network_interface_sg_attachment" "openvpn_sg_attachment" {
-  security_group_id    = aws_security_group.openvpn.id
-  network_interface_id = aws_network_interface.openvpn.id
-}
-
 //NAT Gateway Network Interface
 resource "aws_network_interface" "nat-gateway" {
   subnet_id   = aws_subnet.rubi-public-subnet.id
@@ -22,12 +16,6 @@ resource "aws_network_interface" "nat-gateway" {
   tags = {
     Name = "NAT Gateway"
   }
-}
-
-//Attach NAT Gateway Security Group
-resource "aws_network_interface_sg_attachment" "nat-gateway_sg_attachment" {
-  security_group_id    = aws_security_group.nat-gateway.id
-  network_interface_id = aws_network_interface.nat-gateway.id
 }
 
 //BookStack Network Interface
@@ -40,12 +28,6 @@ resource "aws_network_interface" "bookstack" {
   }
 }
 
-//Attach BookStack Security Group
-resource "aws_network_interface_sg_attachment" "bookstack_sg_attachment" {
-  security_group_id    = aws_security_group.bookstack.id
-  network_interface_id = aws_network_interface.bookstack.id
-}
-
 //Valheim Network Interface
 resource "aws_network_interface" "valheim" {
   subnet_id   = aws_subnet.rubi-public-subnet.id
@@ -54,10 +36,4 @@ resource "aws_network_interface" "valheim" {
   tags = {
     Name = "Valheim"
   }
-}
-
-//Attach Valheim Security Group
-resource "aws_network_interface_sg_attachment" "valheim_sg_attachment" {
-  security_group_id    = aws_security_group.valheim.id
-  network_interface_id = aws_network_interface.valheim.id
 }
